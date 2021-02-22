c7abe7ecbc38a70f66dfecb7fd8efb5d7d656be7
az-shen <azshen@berkeley.edu>
2021-02-21 21:29:13 -0800

    Create music_play_en.java

---
+++ music_play_en.java
@@ -0,0 +1,38 @@
+#JSGF V1.0 utf-8 en; 
+grammar music_play;
+
+public <music_play> =
+	<request> <music>; 
+
+<request> = 
+	[can you] (put on | play [me]) | 
+	I want to listen to; 
+
+<music> = 
+	<artist> | 
+	<song> [by <artist>] | 
+	<genre> [music] | 
+	<album> [by <artist>]; 
+
+<artist> = 
+	beatles | 
+	radiohead |
+	cake |
+	pink floyd; 
+
+<song>   =
+    	comfortably numb |
+	paranoid android |
+	let it be | 
+	hey jude; 
+
+<genre> =
+	jazz |
+	rap |
+	rock; 
+
+<album> =
+	ummagumma | 
+	abbey road |
+	ok computer;
+
